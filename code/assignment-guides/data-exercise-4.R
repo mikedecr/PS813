35b62d5f1a7415474abd248d6229f677a34e85ce
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2020-04-16 16:26:23 -0500

    ex 4 helper script (with my program)

---
+++ code/assignment-guides/data-exercise-4.R
@@ -0,0 +1,95 @@
+# ----------------------------------------------------
+#   Data Exercise 4: 
+#   Code to get started
+#   Provided by Michael DeCrescenzo, Spring 2020
+# ----------------------------------------------------
+
+library("tidyverse")
+library("broom")
+
+source("https://raw.githubusercontent.com/mikedecr/PS813/master/code/programs/create-exercise-4.R")
+
+d4 <- create_exercise_4(456) %>% print()
+
+# estimating a logit with glm()
+# ("generalized linear model")
+mod <- glm(
+  Probat ~ Take + Report + Night + Convict,
+  family = binomial(link = "logit"),
+  data = d4
+)
+
+tidy(mod)
+
+# here is the gist of a logit model:
+# y is binary {0, 1} with "success" probability p:
+#   y ~ Bernoulli(p) 
+
+# we don't predict p directly; we predict a nonlinear function of p
+#   called the "log odds"
+#   odds: p / (1 - p)
+#   log odds: log(p / (1 - p))
+#   (also called the "logit" of p)
+#   model says: logit(p) = XB 
+
+# ==> p isn't linearly related to X, but logit(p) is
+
+# can rewrite as
+#   p = invlogit(XB)
+#   as in, "inverse logit"
+#   or, p = 1 / (1 + exp(-XB))
+# The inverse logit is sometimes called the "logistic" function.
+# It is the CDF of the standard logistic distribution.
+
+# All generalized linear models have this same basic format:
+# Data are draws from some distribution: y ~ D(mu)
+# Parameters of the distribution (for ex. the mean) 
+#   are predicted w/ covariates.
+#   g(mu) = XB
+#   where g() could be some transformation of the parameter 
+#   (called a "link function")
+# in MLE, coefficients B are estimated by maximizing 
+#   the log-likelihood LL(Y; mu) w.r.t the coefficients B
+
+# for OLS, y ~ Normal(mu, sigma)
+# and g(mu) = XB where g(x) = x
+# a.k.a. the "identity link" (i.e. no transformation)
+
+# for logit, y ~ bernoulli(mu)
+# logit(mu) = XB
+
+
+# ---- predictions from logit -----------------------
+
+# the model makes LINEAR PREDICTIONS on the logit scale.
+# In order to transform them into probabilities, we must transform back.
+
+# a demonstration
+# simple predictions:
+logit_predictions <- augment(mod) %>%
+  print()
+
+# notice that the predictions are not in (0, 1)
+ggplot(logit_predictions) +
+  aes(x = .fitted) +
+  geom_histogram()
+
+# if we wanted predictions on the probability scale,
+#   we predict on the log scale,
+#   then apply inverse-logit (logistic CDF)
+# use plogis(): cumulative probability of the logistic distribution
+
+# demonstration: scatter log odds (x) and predicted probability (y)
+# with 95% CIs
+# (CIs don't look smooth in this example because .se.fit depends on X data)
+augment(mod) %>%
+  mutate(
+    .prob = plogis(.fitted),
+    conf.low = plogis(.fitted - (qnorm(.975) * .se.fit)),
+    conf.high = plogis(.fitted + (qnorm(.975) * .se.fit)),
+  ) %>%
+  ggplot() +
+  aes(x = .fitted, y = .prob) +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high))
+
+
--- code/programs/create-exercise-4_stata.do
+++ code/programs/create-exercise-4_stata.do
@@ -1,10 +1,12 @@
-/* This file installs Dave's program,
-   creates the covariate data 
-   (from a fixed seed in the program; the seed in this file does not matter)
-   and then saves the data.
+/* 
+   This file installs Dave's program,
+     creates the covariate data 
+     (from a fixed seed in the program;
+      the seed in this file does not matter)
+     and then saves the data.
 
    The R program then pulls the stata data from Github, 
-   and simulates the DV according to the user's seed num
+     and simulates the DV according to the user's seed num
 */
 
 net install PS813_EX4, from(https://faculty.polisci.wisc.edu/weimer)
